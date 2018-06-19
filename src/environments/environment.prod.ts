f941cef67438debaa6a0c72850113ab28f9af6f6
Renan Sigolo <renan.sigolo@gmail.com>
2018-06-20 00:40:17 +1000

    env changed to lead-app project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
 	production: true,
 	version: 'v.1.0.0 beta',
 	firebase: {
-		apiKey: 'AIzaSyCUJMRYmjbzCCFKfm--Go7rX00_h9qWwfk',
-		authDomain: 'kent-ac75b.firebaseapp.com',
-		databaseURL: 'https://kent-ac75b.firebaseio.com',
-		projectId: 'kent-ac75b',
-		storageBucket: 'kent-ac75b.appspot.com',
-		messagingSenderId: '19289947337'
+		apiKey: "AIzaSyDh6FBRemygTT5Qy-yZD6A0B07gDgjrlG0",
+    authDomain: "lead-app-514e3.firebaseapp.com",
+    databaseURL: "https://lead-app-514e3.firebaseio.com",
+    projectId: "lead-app-514e3",
+    storageBucket: "lead-app-514e3.appspot.com",
+    messagingSenderId: "500998961888"
 	}
 }
