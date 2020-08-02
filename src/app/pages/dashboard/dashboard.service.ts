8a6b7315df4f00b663f54fc0e6fa49a2c5ea209d
Renan Sigolo <renan.sigolo@gmail.com>
2020-08-02 17:27:49 +1000

    fix: linting issues

--- src/app/pages/dashboard/dashboard.service.ts
+++ src/app/pages/dashboard/dashboard.service.ts
@@ -10,7 +10,7 @@ import { User } from '@interfaces/user'
 import { FirestoreService } from '@services/firestore.service'
 import { ToastService } from '@services/toast.service'
 
-@Directive()
+// @Directive()
 @Injectable({
   providedIn: 'root',
 })
